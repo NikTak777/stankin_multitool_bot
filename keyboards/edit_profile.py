657f8b5faefeed01764920b3564ec954a4ba77ee
NikTak777 <nikkondrakhin@gmail.com>
2025-08-20 19:39:43 +0300

    Обновление меню редактирования профиля

--- keyboards/edit_profile.py
+++ keyboards/edit_profile.py
@@ -5,7 +5,7 @@ def get_edit_profile_inline_keyboard():
         [InlineKeyboardButton(text="Указать номер группы", callback_data="set_group")],
         [InlineKeyboardButton(text="Указать дату дня рождения", callback_data="start_birthdate_input")],
         [InlineKeyboardButton(text="Указать новый никнейм", callback_data="set_nickname")],
-        [InlineKeyboardButton(text="Ввести новый вишлист", callback_data="set_wishlist")],
+        [InlineKeyboardButton(text="Ввести новый вишлист", callback_data="my_wishlist")],
         [InlineKeyboardButton(text="⬅️ Назад", callback_data="start")]
     ])
     return keyboard
