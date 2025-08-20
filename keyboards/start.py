2a3b60a94fe21dce8f01fa8626c6a92bca3cfce0
NikTak777 <nikkondrakhin@gmail.com>
2025-08-20 17:10:23 +0300

    Добавлена клавиатура для декодера require_birthdate

--- keyboards/start.py
+++ keyboards/start.py
@@ -3,8 +3,8 @@ from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 def get_start_inline_keyboard():
     keyboard = InlineKeyboardMarkup(inline_keyboard=[
         [InlineKeyboardButton(text="Посмотреть расписание", callback_data="view_schedule")],
-        [InlineKeyboardButton(text="Показать информацию об аккаунте", callback_data="info_account")],
-        [InlineKeyboardButton(text="Посмотреть вишлист друга", callback_data="view_friend_wishlist")],
+        [InlineKeyboardButton(text="Показать информацию об аккаунте", callback_data="info")],
+        [InlineKeyboardButton(text="Посмотреть вишлист друга", callback_data="friend_wishlist")],
         [InlineKeyboardButton(text="Редактировать профиль >>>", callback_data="edit_profile_menu")]
     ])
     return keyboard
