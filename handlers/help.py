fdd845fb3e0937e24498f66511f5f6a127848257
NikTak777 <nikkondrakhin@gmail.com>
2025-08-22 22:13:55 +0300

    Обновлён хендлер help
    Добавлена поддержка инлайн кнопок

--- handlers/help.py
+++ handlers/help.py
@@ -1,10 +1,13 @@
 # handlers/help
 
-from aiogram import Router, types
+from aiogram import Router, types, F
 from aiogram.filters import Command
+from aiogram.types import CallbackQuery
 
 from utils.logger import write_user_log
 from utils.user_utils import get_user_name
+
+from keyboards.back_to_menu import get_back_inline_keyboard
 
 # Декораторы
 from decorators.private_only import private_only
@@ -13,18 +16,49 @@ from decorators.ensure_user_in_db import ensure_user_in_db
 
 router = Router()
 
+
+# Обработчик команды /help
 @router.message(Command("help"))
 @private_only
 @ensure_user_in_db
 @sync_username
-async def help_msg(message: types.Message):
-
-    UserNAME = await get_user_name(message)
+async def cmd_help(message: types.Message):
+    full_name = await get_user_name(message)
+    help_message = await get_help_text(full_name)
 
     await message.answer(
-        f"Привет, {UserNAME}!\n\n"
-        "Я — бот Stankin-MultiTool! 🤖\n"
-        "Моя задача — помогать студентам Станкина: поздравлять их с днём рождения 🎉 и отправлять актуальное расписание занятий 📅."
+        text=help_message,
+        reply_markup=get_back_inline_keyboard(),
+        parse_mode="Markdown"
     )
-    msg = f"Пользователь {message.from_user.full_name} ({message.from_user.id}) запросил информацию о боте"
-    write_user_log(msg)
+
+    write_user_log(
+        f"Пользователь {message.from_user.full_name} ({message.from_user.id}) запросил информацию о боте"
+    )
+
+
+# Обработчик инлайн-кнопки
+@router.callback_query(F.data == "help")
+async def callback_help(callback: CallbackQuery):
+    full_name = await get_user_name(callback.from_user)
+    help_message = await get_help_text(full_name)
+
+    await callback.message.edit_text(
+        text=help_message,
+        reply_markup=get_back_inline_keyboard(),
+        parse_mode="Markdown"
+    )
+    await callback.answer()
+
+    write_user_log(
+        f"Пользователь {callback.from_user.full_name} ({callback.from_user.id}) запросил информацию о боте"
+    )
+
+
+async def get_help_text(full_name: str) -> str:
+    return (
+        f"Привет, {full_name}!\n\n"
+        "Я — бот Stankin-MultiTool! 🤖\n"
+        "Моя задача — помогать студентам Станкина: поздравлять их с днём рождения 🎉 и отправлять актуальное расписание занятий 📅.\n\n"
+        "Более подробную информацию о том, что я умею и как устроен, вы можете найти в [GitHub-репозитории](https://github.com/NikTak777/stankin_multitool_bot/)"
+    )
