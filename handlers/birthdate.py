259bd46d13b94c8c02a390e9036a5b562b8fd279
NikTak777 <nikkondrakhin@gmail.com>
2025-08-24 12:32:00 +0300

    Обновление обработчика команды /birthday

--- handlers/birthdate.py
+++ handlers/birthdate.py
@@ -1,21 +1,23 @@
 from aiogram import types, Router
 from aiogram.filters import Command
-from aiogram.fsm.context import FSMContext
-from aiogram.types import CallbackQuery, ReplyKeyboardRemove
 
 from utils.logger import write_user_log
-from utils.database import get_user_info, check_user_exists
 from utils.user_utils import get_user_name
 from keyboards.birthdate import get_birthdate_inline_keyboard
+
+# Декораторы
+from decorators.private_only import private_only
+from decorators.sync_username import sync_username
+from decorators.ensure_user_in_db import ensure_user_in_db
 
 router = Router()
 
 # Обработчик команды /birthdate
 @router.message(Command("birthdate"))
+@private_only
+@ensure_user_in_db
+@sync_username
 async def birthdate_msg(message: types.Message):
-    if message.chat.type != "private":
-        await message.answer("Эта команда доступна только в личной переписке с ботом.")
-        return
 
     msg = f"Пользователь {message.from_user.full_name} ({message.from_user.id}) ввёл команду /birthdate"
     write_user_log(msg)
