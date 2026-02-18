8e6850f0d1e014211ec495be208107ca5582aef3
NikTak777 <nikkondrakhin@gmail.com>
2026-02-19 00:49:16 +0300

    Обновление файлов состояний

--- states/friends_states.py
+++ states/friends_states.py
@@ -3,3 +3,7 @@ from aiogram.fsm.state import StatesGroup, State
 
 class EditMenuState(StatesGroup):
     editing = State()
+
+
+class WishlistSuggestionState(StatesGroup):
+    awaiting_wishlist = State()
