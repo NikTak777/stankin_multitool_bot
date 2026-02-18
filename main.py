2881a70cceec17fa3e796a8fb3d78b1100c1e66a
NikTak777 <nikkondrakhin@gmail.com>
2026-02-19 00:34:11 +0300

    Обновление файлов запуска и создания экземпляра бота

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
-# main.py
 from bot import bot, dp
 import asyncio
 
 from handlers import (start_menu, info, birthdate, group_registration, group_panel, edit_profile,
                       other_profile, user_wishlist, user_group, schedule, help, user_nickname,
-                      update, admin_panel)
-from handlers.friends import friends_menu, friends_request, friends_edit_menu, delete_friend, friend_profile
+                      update, admin_panel, statistics)
+from handlers.friends import friends_menu, friends_request, friends_edit_menu, delete_friend, friend_profile, wishlist_suggestion
 
-from handlers.schedule_modules import other_group
+from handlers.schedule_modules import other_group, friend
 
 from utils.logger import write_user_log
 from utils import set_user_birthdate
@@ -15,13 +14,15 @@ from utils.database_utils.init_database import init_database
 
 from tasks.daily_schedule import send_daily_schedule
 from tasks.birthday_notifications import check_birthdays
+from tasks.new_year_greetings import check_new_year
+from tasks.schedule_notifications import check_schedule_notifications
 
 from middlewares.user_activity import ActivityMiddleware
 
 # Отключение ненужных логов от aiogram
 # logging.getLogger("aiogram.event").setLevel(logging.WARNING)
 
-# Подключаем роутеры
+# Подключение роутеров
 dp.update.middleware(ActivityMiddleware())
 dp.include_router(start_menu.router)
 dp.include_router(info.router)
@@ -38,23 +39,28 @@ dp.include_router(help.router)
 dp.include_router(user_nickname.router)
 dp.include_router(update.router)
 dp.include_router(admin_panel.router)
+dp.include_router(statistics.router)
 dp.include_router(friends_request.router)
 dp.include_router(friends_menu.router)
 dp.include_router(friends_edit_menu.router)
 dp.include_router(delete_friend.router)
 dp.include_router(friend_profile.router)
+dp.include_router(wishlist_suggestion.router)
 dp.include_router(other_group.router)
+dp.include_router(friend.router)
 
 
 async def main():
     """Запуск бота"""
-    write_user_log("Бот запущен!")  # Логирует запуск
+    write_user_log("Бот запущен!")
 
     init_database()
 
     # запускаем рассылку параллельно с ботом
     asyncio.create_task(send_daily_schedule())
     asyncio.create_task(check_birthdays())
+    asyncio.create_task(check_new_year())
+    asyncio.create_task(check_schedule_notifications())
 
     await dp.start_polling(bot)
 
