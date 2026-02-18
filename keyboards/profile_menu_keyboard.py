3757321fc1914834b78f4496473995b6d1b6f5b2
NikTak777 <nikkondrakhin@gmail.com>
2026-02-19 01:50:11 +0300

    Изменение клавиатур бота
    Перенос кнопки открытия меню Статистика в профиль

--- keyboards/profile_menu_keyboard.py
+++ keyboards/profile_menu_keyboard.py
@@ -3,6 +3,7 @@ from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 def get_profile_menu_inline_keyboard():
     keyboard = InlineKeyboardMarkup(inline_keyboard=[
         [InlineKeyboardButton(text="✏️ Редактировать", callback_data="edit_profile_menu")],
+        [InlineKeyboardButton(text="📊 Статистика", callback_data="statistics")],
         [InlineKeyboardButton(text="⬅️ Назад", callback_data="start")]
     ])
     return keyboard
