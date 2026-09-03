0706763c97ed4a44a7a2388b8eee9c3f0a1de065
Nikita Kondrakhin <nikkondrakhin@gmail.com>
2026-09-03 15:02:17 +0300

    Oбновление FSM для ввода номера своей группы

--- states/group_state.py
+++ states/group_state.py
@@ -1,5 +1,6 @@
 from aiogram.fsm.state import State, StatesGroup
 
-class GroupState(StatesGroup):
-    waiting_for_group = State()
-    waiting_for_subgroup = State()
+class GroupSelectState(StatesGroup):
+    choosing_code = State()
+    choosing_year = State()
+    choosing_group = State()
