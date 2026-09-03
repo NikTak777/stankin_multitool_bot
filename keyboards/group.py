20cfe32de1dbc36a454f1b5c5959995175e74b59
Nikita Kondrakhin <nikkondrakhin@gmail.com>
2026-09-03 18:55:49 +0300

    добавлена клавиатура для выбора года группы

--- keyboards/group.py
+++ keyboards/group.py
@@ -1,7 +1,7 @@
 from aiogram.types import InlineKeyboardMarkup
 from aiogram.utils.keyboard import InlineKeyboardBuilder
 
-from services.group import get_available_group_codes
+from services.group import get_available_group_codes, get_available_group_years
 
 async def get_enter_code_group_keyboard() -> InlineKeyboardMarkup:
     codes = await get_available_group_codes()
@@ -9,4 +9,15 @@ async def get_enter_code_group_keyboard() -> InlineKeyboardMarkup:
     builder = InlineKeyboardBuilder()
     for code in codes['data']:
         builder.button(text=code, callback_data=f"group_code_{code}")
+    builder.adjust(3)
+    return builder.as_markup()
+
+
+async def get_select_year_group_keyboard(user_group_code: str) -> InlineKeyboardMarkup:
+    years = await get_available_group_years(user_group_code)
+
+    builder = InlineKeyboardBuilder()
+    for year in years['data']:
+        builder.button(text=year, callback_data=f"group_year_{year}")
+    builder.adjust(3)
     return builder.as_markup()
