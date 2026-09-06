fe5ecf4922e5dd28c07e1dc881727c7ea3c4a036
Nikita Kondrakhin <nikkondrakhin@gmail.com>
2026-09-06 13:56:28 +0300

    Добавлена общая функция перевода даты из строки

--- utils/time.py
+++ utils/time.py
@@ -6,3 +6,7 @@ MOSCOW_TZ = ZoneInfo("Europe/Moscow")
 def get_now_time() -> datetime:
     """Возвращает текущее московское время."""
     return datetime.now(tz=MOSCOW_TZ)
+
+def format_str_to_datetime(str_time: str) -> datetime:
+    """Возвращает переведённую дату из строки формата DD-MM-YYYY."""
+    return datetime.strptime(str_time, "%d-%m-%Y")
