da31d1884aea8deb46abf55c1f2084d68f7631c7
Nikita Kondrakhin <nikkondrakhin@gmail.com>
2026-09-11 18:43:44 +0300

    Обновление логера. Пишется московское время

--- utils/logger.py
+++ utils/logger.py
@@ -1,42 +1,34 @@
-import logging
-from datetime import datetime
-import pytz
 import os
+import logging
 
-# Часовой пояс Москвы
-tz_moscow = pytz.timezone("Europe/Moscow")
+from utils.time import get_now_time
 
-# Создаём папку для логов, если её нет
 LOG_DIR = "logs"
 os.makedirs(LOG_DIR, exist_ok=True)
-
-# Конфигурируем логирование
 log_file = os.path.join(LOG_DIR, "bot.log")
 
+logging.Formatter.converter = lambda *args: get_now_time().timetuple()
+
 logging.basicConfig(
-    level=logging.INFO,  # Можно менять на DEBUG, WARNING, ERROR
+    level=logging.INFO,
     format="%(asctime)s [%(levelname)s] %(message)s",
     datefmt="%Y-%m-%d %H:%M:%S",
     handlers=[
-        logging.FileHandler(log_file, encoding="utf-8"),  # Логирование в файл
-        logging.StreamHandler()  # Логирование в консоль
+        logging.FileHandler(log_file, encoding="utf-8"),
+        logging.StreamHandler()
     ],
 )
 
-# Функция логирования
+
 def write_user_log(message: str, level: str = "info"):
     """Логирует действия пользователей и работу бота."""
-    # timestamp = datetime.now(tz=tz_moscow).strftime("%Y-%m-%d %H:%M:%S")
-    log_message = f"{message}"
-
-    # Выбор уровня логирования
     if level == "info":
-        logging.info(log_message)
+        logging.info(message)
     elif level == "warning":
-        logging.warning(log_message)
+        logging.warning(message)
     elif level == "error":
-        logging.error(log_message)
+        logging.error(message)
     elif level == "critical":
-        logging.critical(log_message)
+        logging.critical(message)
     else:
-        logging.debug(log_message)
+        logging.debug(message)
