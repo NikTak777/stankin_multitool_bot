0ca4fda11d70f905cf679df857ed8849fc18643b
Nikita Kondrakhin <nikkondrakhin@gmail.com>
2026-09-14 20:53:09 +0300

    Удалён старый роутер поиска людей

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from bot import bot, dp
 import asyncio
 
 from handlers import (start_menu, info, birthdate, group_registration, group_panel, edit_profile,
-                      other_profile, user_wishlist, user_group, schedule, professor_schedule, help,
+                      user_wishlist, user_group, schedule, professor_schedule, help,
                       user_nickname, update, statistics)
 from handlers.friends import friends_menu, friends_edit_menu, delete_friend, friend_profile, wishlist_suggestion
 
@@ -35,7 +35,6 @@ dp.include_router(set_user_birthdate.router)
 dp.include_router(group_registration.router)
 dp.include_router(group_panel.router)
 dp.include_router(edit_profile.router)
-dp.include_router(other_profile.router)
 dp.include_router(user_wishlist.router)
 dp.include_router(user_group.router)
 dp.include_router(professor_schedule.router)
