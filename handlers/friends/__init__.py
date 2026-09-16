aa76ff2e8fb5cce1b3ac8fc548ea428d45ad6de8
Nikita Kondrakhin <nikkondrakhin@gmail.com>
2026-09-16 20:04:41 +0300

    Подключения роутера удаления друзей к общему роутеру друзей

--- handlers/friends/__init__.py
+++ handlers/friends/__init__.py
@@ -1,9 +1,11 @@
 from aiogram import Router
 
 from .request import request_router
+from .delete import delete_router
 
 friends_router = Router()
 
 friends_router.include_routers(
-    request_router
+    request_router,
+    delete_router
 )
--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import asyncio
 from handlers import (start_menu, info, birthdate, group_registration, group_panel, edit_profile,
                       other_profile, user_wishlist, user_group, schedule, professor_schedule, help,
                       user_nickname, update, statistics)
-from handlers.friends import friends_menu, friends_edit_menu, delete_friend, friend_profile, wishlist_suggestion
+from handlers.friends import friends_menu, friends_edit_menu, friend_profile, wishlist_suggestion
 
 from handlers.schedule_modules import other_group, friend
 from handlers.contest import contest_router
@@ -46,7 +46,6 @@ dp.include_router(update.router)
 dp.include_router(statistics.router)
 dp.include_router(friends_menu.router)
 dp.include_router(friends_edit_menu.router)
-dp.include_router(delete_friend.router)
 dp.include_router(friend_profile.router)
 dp.include_router(wishlist_suggestion.router)
 dp.include_router(other_group.router)
