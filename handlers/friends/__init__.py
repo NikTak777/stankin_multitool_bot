f878d404e9a953882fa7068429bf8e6580e0cb0e
Nikita Kondrakhin <nikkondrakhin@gmail.com>
2026-09-17 01:02:53 +0300

    добавлен новый роутер просмотра профиля друга

--- handlers/friends/__init__.py
+++ handlers/friends/__init__.py
@@ -2,10 +2,12 @@ from aiogram import Router
 
 from .request import request_router
 from .delete import delete_router
+from .friend_profile import profile_router
 
 friends_router = Router()
 
 friends_router.include_routers(
     request_router,
-    delete_router
+    delete_router,
+    profile_router
 )
