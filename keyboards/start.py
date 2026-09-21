6aaf4693738e1409add74e645ac9c4e353ee56eb
Nikita Kondrakhin <nikkondrakhin@gmail.com>
2026-09-21 22:00:19 +0300

    Удалён старый файл хендлеров выбора группы. К стартовой клавиатуре подключен новый профиль

--- keyboards/start.py
+++ keyboards/start.py
@@ -9,7 +9,7 @@ def get_start_inline_keyboard(
     keyboard = [
         [InlineKeyboardButton(text="📅 Расписание", callback_data="schedule")],
         [InlineKeyboardButton(text="👨‍🏫 Расписание преподавателя", callback_data="professor_schedule_open")],
-        [InlineKeyboardButton(text="👤 Профиль", callback_data="info")],
+        [InlineKeyboardButton(text="👤 Профиль", callback_data="profile-info")],
         [InlineKeyboardButton(text="🤝 Друзья", callback_data="friends_menu")],
         [InlineKeyboardButton(text="🔎 Поиск людей", callback_data="search_profile")],
         [InlineKeyboardButton(text="❓ FAQ", callback_data="help")]
