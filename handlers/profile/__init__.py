f7838229c4ff1a455e16fb2e1554eef0c6550967
Nikita Kondrakhin <nikkondrakhin@gmail.com>
2026-09-21 21:56:41 +0300

    Подключен новый роутер редактирования профиля

--- handlers/profile/__init__.py
+++ handlers/profile/__init__.py
@@ -3,11 +3,13 @@ from aiogram import Router
 from .info import info_router
 from .menu import menu_router
 from .statistics import stats_router
+from .edit import edit_router
 
 profile_router = Router()
 
 profile_router.include_routers(
     info_router,
     stats_router,
-    menu_router
+    menu_router,
+    edit_router
 )
